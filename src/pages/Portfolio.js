5bef936a6d166f4acb045de66af05f82fecbb1b8
darnocer <darnocer@gmail.com>
2020-08-22 19:26:33 -0600

    heading styling

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -34,24 +34,24 @@ class Portfolio extends Component {
       <section id="portfolio">
         <div className="row">
           <div className="twelve columns collapsed">
-            <h1>Development Project Highlights</h1>
+            <h1>Development Projects</h1>
             <div
               id="portfolio-wrapper"
               className="bgrid-thirds s-bgrid-thirds cf">
               {projects}
             </div>
-            <h1>
+            <h2>
               More projects available on{" "}
               <a href="https://github.com/darnocer" target="_blank">
                 Github
               </a>
-            </h1>
-            <h1>
+            </h2>
+            <h2>
               Personal documentation compilation Project:&nbsp;
               <a href="https://github.com/darnocers-tips" target="_blank">
                 THE VAULT
               </a>
-            </h1>
+            </h2>
           </div>
         </div>
       </section>

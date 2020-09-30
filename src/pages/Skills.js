aec28aaf4182f6b3cf0e8901b803b2fbfac66c89
darnocer <darnocer@gmail.com>
2020-09-30 11:54:19 -0600

    converting skill bars to technologies

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Container, Row, Col } from "reactstrap";
 
 class Skills extends Component {
   render() {
@@ -19,40 +18,51 @@ class Skills extends Component {
 
     return (
       <section id="skills">
-        <h1>Skills</h1>
-        <table>
-          <tbody>
-            <tr>
-              <td>
-                <h2>
-                  Front-End <i className="fas fa-code"></i>
-                </h2>
-                HTML5, CSS3, React.js, javascript, jQuery, Bootstrap and other
-                CSS frameworks, RESTful APIs
-              </td>
-              <td>
-                <h2>
-                  Back-End <i className="fas fa-terminal"></i>
-                </h2>
-                SQL (MySQL), NoSQL(MongoDB), Node.js, Express.js
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <h2>
-                  Design <i className="fas fa-paint-brush"></i>
-                </h2>
-                SquareSpace, GoDaddy, UI/UX, Adobe Illustrator
-              </td>
-              <td>
-                <h2>
-                  Other Tools <i className="fas fa-code-branch"></i>
-                </h2>
-                Visual Studio Code, GitHub, Git, Microsoft Suite,
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="table-container">
+          <h1>Skills</h1>
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <h2>
+                    Front-End <i className="fas fa-code"></i>
+                  </h2>
+                  <p>
+                    HTML5 • CSS3 • React.js • javascript • jQuery • Handlebars •
+                    Bootstrap and other CSS frameworks
+                  </p>
+                </td>
+                <td>
+                  <h2>
+                    Back-End <i className="fas fa-terminal"></i>
+                  </h2>
+                  <p>
+                    SQL (MySQL) • NoSQL (MongoDB) • Node.js • Express.js •
+                    Mongoose
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h2>
+                    Design <i className="fas fa-paint-brush"></i>
+                  </h2>
+                  <p>
+                    SquareSpace • GoDaddy • UI/UX heuristics and principles •
+                    Adobe Illustrator • Adobe Photoshop • Balsamiq
+                  </p>
+                </td>
+                <td>
+                  <h2>
+                    Other Tools <i className="fas fa-code-branch"></i>
+                  </h2>
+                  Visual Studio Code • GitHub • Git • Microsoft Suite • RESTful
+                  APIs • AJAX • JSON • Heroku • Netlify
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </section>
     );
   }

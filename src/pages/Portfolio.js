5826ded0a5a50d2e785b2876256407d365b3da5b
darnocer <darnocer@gmail.com>
2020-10-09 14:12:35 -0600

    defaulting project filter to development and removing all

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 function Portfolio(props) {
-  const [filter, setFilter] = useState("all");
+  const [filter, setFilter] = useState("development");
   const [projects, setProjects] = useState([]);
   const [filteredProjects, setFilteredProjects] = useState([]);
 
@@ -27,33 +27,35 @@ function Portfolio(props) {
     var projectCards =
       filteredProjects && filteredProjects.length
         ? filteredProjects.map(function (p) {
-            var projectImage = "images/portfolio/" + p.image;
-            return (
-              <div key={p.title} className="columns portfolio-item">
-                <div className="item-wrap">
-                  <img alt={p.title} src={projectImage} />
-                  <div className="overlay">
-                    <div className="portfolio-item-meta">
-                      <h5>{p.title}</h5>
-                      <p>{p.description}</p>
+            if (p.category.includes(filter)) {
+              var projectImage = "images/portfolio/" + p.image;
+              return (
+                <div key={p.title} className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <img alt={p.title} src={projectImage} />
+                    <div className="overlay">
+                      <div className="portfolio-item-meta">
+                        <h5>{p.title}</h5>
+                        <p>{p.description}</p>
+                      </div>
+                    </div>
+                    <div className="link-icon">
+                      {p.url ? (
+                        <a href={p.url} title={p.title} target="_blank">
+                          <i className="fa fa-link fa-lg"></i>
+                        </a>
+                      ) : null}
+                      &nbsp;&nbsp;&nbsp;
+                      {p.source ? (
+                        <a href={p.source} title="Source Code" target="_blank">
+                          <i className="fas fa-code fa-lg"></i>
+                        </a>
+                      ) : null}
                     </div>
                   </div>
-                  <div className="link-icon">
-                    {p.url ? (
-                      <a href={p.url} title={p.title} target="_blank">
-                        <i className="fa fa-link fa-lg"></i>
-                      </a>
-                    ) : null}
-                    &nbsp;&nbsp;&nbsp;
-                    {p.source ? (
-                      <a href={p.source} title="Source Code" target="_blank">
-                        <i className="fas fa-code fa-lg"></i>
-                      </a>
-                    ) : null}
-                  </div>
                 </div>
-              </div>
-            );
+              );
+            }
           })
         : [];
   }
@@ -64,21 +66,21 @@ function Portfolio(props) {
         <div className="row">
           <div className="twelve columns collapsed">
             <div className="filters">
-              <h1>Projects</h1>
-              <a
-                href="/#portfolio"
-                className={filter === "all" ? "active" : null}
-                onClick={() => setFilter("all")}>
-                All
-              </a>{" "}
-              <span className="separator">|</span>
+              <h1>Portfolio</h1>
               <a
                 href="/#portfolio"
                 className={filter === "development" ? "active" : null}
                 onClick={() => setFilter("development")}>
                 Development
               </a>{" "}
-              <span className="separator">|</span>
+              <span className="separator">&nbsp;|&nbsp;</span>
+              <a
+                href="/#portfolio"
+                className={filter === "fullstack" ? "active" : null}
+                onClick={() => setFilter("fullstack")}>
+                Full Stack
+              </a>
+              <span className="separator">&nbsp;|&nbsp;</span>
               <a
                 href="/#portfolio"
                 className={filter === "design" ? "active" : null}

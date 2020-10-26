5c78fd36d7273ebd3be7fc41a66bf622efcb8c9a
darnocer <darnocer@gmail.com>
2020-10-26 11:51:39 -0600

    fonts

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -63,18 +63,18 @@ class Header extends Component {
         </nav>
 
         <div className="row banner">
-          <div className="banner-bg">
-            <div className="banner-text">
-              <h1 className="responsive-headline">{fullname}</h1>
+          <div className="banner-text">
+            {/* <div className="banner-bg"> */}
+            <h1 className="responsive-headline">{fullname}</h1>
 
-              <h3>
-                I'm a {city}-based <span>{occupation}</span>. {description}
-              </h3>
-            </div>
-            <hr />
-            <ul className="social">{networks}</ul>
+            <h3>
+              I'm a {city}-based <span>{occupation}</span>. {description}
+            </h3>
           </div>
+          <hr />
+          <ul className="social">{networks}</ul>
         </div>
+        {/* </div> */}
 
         <p className="scrolldown">
           <a className="smoothscroll" href="#about">
